c472f7fc2abcb864ce2c7cb5f429c8f4ca8bf709
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-09-12 18:07:24 +0300

    completed tasks 1.6-1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Header = (props) =>{
+  return(
+  <>
+    <h1>{props.title}</h1>
+  </>
+  )
+} 
+
+const Button = (props) =>{
+ 
+return(
+  <>
+   <button onClick={props.button.function}>{props.button.text}</button>
+  </>
+)
 }
 
-export default App;
+
+const VoteButtons = (props) =>{
+  
+  return(
+    <>
+      <Button button={props.buttons[0]}/>
+      <Button button={props.buttons[1]}/>
+      <Button button={props.buttons[2]}/>
+    </>
+  )
+}
+
+const StatisticsLine = (props) =>{
+  return(
+  <tr>
+    <td>{props.text}</td>
+    <td> {props.value}</td>
+  </tr>
+  )
+}
+
+
+const Statistics = (props) =>{
+  const calculateAvg = () =>{
+    const sum = props.good - props.bad
+    const avg = sum / props.count
+    return avg
+  }
+  
+  const calculateGoodPercent = () => {
+    return props.good / props.count * 100
+  }
+  if(props.count > 0){
+    return(
+      <>
+      <table>
+        <tbody>
+        <StatisticsLine text={"good"} value={props.good}/>
+        <StatisticsLine text={"neutral"} value={props.neutral}/>
+        <StatisticsLine text={"bad"} value={props.bad}/>
+        <StatisticsLine text={"average"} value={calculateAvg()}/>
+        <StatisticsLine text={"positive"} value={calculateGoodPercent()}/>
+        </tbody>
+      </table>
+     
+      </>
+     )
+  }
+  else{
+    return(
+      <>
+        <p>No feedback given</p>
+      </>
+     )
+  }
+}
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [count, setCount] = useState(0)
+  
+  const increaseGood = () =>{
+    setGood(good + 1)
+    setCount(count + 1)
+  }
+
+  const increaseNeutral = () =>{
+    setNeutral(neutral + 1)
+    setCount(count + 1)
+ }
+
+ const increaseBad = () =>{
+  setBad(bad + 1)
+  setCount(count + 1)
+}
+
+const buttons = [
+  {
+    function : increaseGood,
+    text : "good"
+  },
+  {
+    function : increaseNeutral,
+    text : "neutral"
+  },
+  {
+    function : increaseBad,
+    text : "bad"
+  }
+]
+
+  return (
+    <>
+      <Header title="give feedback"/>
+      <VoteButtons buttons={buttons}/>
+      <Header title="statistics"/>
+      <Statistics good={good} neutral={neutral} bad={bad} count={count}/>
+    </>
+  )
+}
+
+export default App
