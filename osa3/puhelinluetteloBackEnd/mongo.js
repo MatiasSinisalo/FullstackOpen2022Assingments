230fd8e5185a13ce2539748c8c51cc597e3b2405
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-09-27 18:13:36 +0300

    completed task 3.12

---
+++ osa3/puhelinluetteloBackEnd/mongo.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+
+
+const addPerson = (name, password) =>{
+    const personsSchema = new mongoose.Schema({
+        name: String,
+        number: String
+      })
+      
+      const Person = mongoose.model('Person', personsSchema)
+      
+      const person = new Person({
+        name: name,
+        number: number
+      })
+      
+      person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+      })
+}
+
+const getPersons = () => {
+    const personsSchema = new mongoose.Schema({
+        name: String,
+        number: String
+      })
+      
+    const Person = mongoose.model('Person', personsSchema)
+    
+    Person.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person =>{
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+    
+}
+
+
+const password = process.argv[2]
+
+const name = process.argv[3]
+
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.koyprz5.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+
+if(name && number){
+    addPerson(name, number);
+}
+else{
+    getPersons();
+}
+
+
+
+
