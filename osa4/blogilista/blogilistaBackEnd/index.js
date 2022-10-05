9ce21c8cc0e11cbc9a561c5414a7cdf865abfcf6
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-10-05 17:53:26 +0300

    moved all routes about blogs to controllers/blogs.js

--- osa4/blogilista/blogilistaBackEnd/index.js
+++ osa4/blogilista/blogilistaBackEnd/index.js
@@ -2,33 +2,17 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const mongoUrl = mongoose.connect(process.env.MONGODB_URI)
+
+
 app.use(cors())
 app.use(express.json())
 
-const mongoose = require('mongoose')
-require('dotenv').config()
-const mongoUrl = 
-mongoose.connect(process.env.MONGODB_URI)
+app.use('/api/blogs', blogsRouter)
 
-const Blog = require('./models/blog')
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
