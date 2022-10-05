7514475ada81f730756a66c132f70ae253716a8e
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-10-05 18:38:31 +0300

    completed task 4.4

--- osa4/blogilista/blogilistaBackEnd/utils/list_helper.js
+++ osa4/blogilista/blogilistaBackEnd/utils/list_helper.js
@@ -3,6 +3,17 @@ const dummy = (blogs) => {
     return 1
   }
   
+const totalLikes = (blogs) => {
+    const counter = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return blogs.reduce(counter, 0)
+}
+
+
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
