ec367129f68f8b5f190d9f8c068045ef71b7b8f8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-10-05 17:58:59 +0300

    moved express app logic to app.js

--- osa4/blogilista/blogilistaBackEnd/index.js
+++ osa4/blogilista/blogilistaBackEnd/index.js
@@ -1,18 +1,7 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const mongoUrl = mongoose.connect(process.env.MONGODB_URI)
+const app = require('./app.js')
 
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
+const server = http.createServer(app)
 
 const PORT = 3003
 app.listen(PORT, () => {
