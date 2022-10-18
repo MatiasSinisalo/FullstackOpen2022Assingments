9269c5588e8e750b1dace8f97801b045e16f223f
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-10-18 14:47:02 +0300

    completed task 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,53 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+
+
+
+const Blog = ({blog}) => {
+  const [fullViewVisible, setFullViewVisible] = useState(false)
+
+  const toggleFullView = () => {
+    setFullViewVisible(!fullViewVisible)
+  }
+
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const preView = () => (
+    <div>
+    <h3>{blog.title} {blog.author} <button onClick={toggleFullView}>view</button></h3>
+    </div>
+  )
+
+  const fullView = () => (
+    <div>
+      <h3>{blog.title} <button onClick={toggleFullView}>hide</button></h3> 
+     
+      <p>{blog.url}</p>
+    
+      <p>likes {blog.likes}</p>
+     
+      <p><b>{blog.author}</b></p>
+
+    </div>
+  )
+  return(
+    <div style={blogStyle}>
+      {
+      !fullViewVisible ?
+      preView()
+      :
+      fullView()
+      }
+    </div>  
+  )
+}
 
 export default Blog
