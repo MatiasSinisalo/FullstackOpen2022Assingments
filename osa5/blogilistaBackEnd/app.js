6e63dfdb12fe51a19c15efb3a810d7afd8c2ed0a
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-10-21 12:33:34 +0300

    created a copy of bloglist back end from part4 to part5

---
+++ osa5/blogilistaBackEnd/app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const tokenExtractor = require('./utils/tokenExtractor')
+const userExtractor = require('./utils/userExtractor')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const mongoose = require('mongoose')
+
+const mongoUrl = mongoose.connect(config.MONGODB_URI)
+
+
+app.use(cors())
+app.use(express.json())
+app.use(tokenExtractor)
+app.use(userExtractor)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+module.exports = app
