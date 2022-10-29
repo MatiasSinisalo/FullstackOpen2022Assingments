b3fefd01add855bbbc8ab39394aebe9bfe759099
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-10-29 16:32:39 +0300

    completed task 6.9

---
+++ osa6/redux-anecdotes/src/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from '@reduxjs/toolkit'
+import anecdoteReducer from './reducers/anecdoteReducer'
+const store = configureStore({reducer: {anecdotes: anecdoteReducer}})
+
+export default store
