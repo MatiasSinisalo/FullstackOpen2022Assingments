fd7a486aaebd829738494531d53578e578d395bb
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-10-31 09:59:07 +0200

    completed task 6.13

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import { createSlice } from "@reduxjs/toolkit"
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,8 +10,14 @@ const asObject = (anecdote) => {
   }
 }
 
+
+const anecdotesAtStart = []
+
+
 const initialState = anecdotesAtStart.map(asObject)
 
+
+/*
 export const createAnecdote = (newAnecdote) => {
   return{
     type: 'create', data: {anecdote: newAnecdote}
@@ -30,7 +29,9 @@ export const voteAnecdote = (id) => {
     type: 'vote', data: {id: id}
   }
 }
+*/
 
+/*
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
@@ -49,5 +50,29 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+*/
 
-export default reducer
+const anecdoteReducer = createSlice( {
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    voteAnecdote(state, action) {
+      console.log(action)
+      const updatedAnecdotes = state.map(anecdote => anecdote.id === action.payload ? {...anecdote, votes : anecdote.votes + 1} : anecdote)
+      .sort((a, b) => {return b.votes - a.votes})
+      console.log(updatedAnecdotes)
+      state = updatedAnecdotes
+      return state
+    },
+    createAnecdote(state, action){
+      console.log(action)
+      return state.concat(asObject(action.payload))
+    }
+  }
+}
+
+)
+
+export const { voteAnecdote, createAnecdote } = anecdoteReducer.actions
+
+export default anecdoteReducer.reducer
--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -1,7 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
-import anecdoteReducer from './reducers/anecdoteReducer'
+import anecdoteReducer, { createAnecdote } from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
+import anecdoteService from "./services/anecdoteService"
+
 const store = configureStore({reducer: {anecdotes: anecdoteReducer, notification: notificationReducer, filter: filterReducer}})
+anecdoteService.getAll().then(
+    anecdotes => {
+        anecdotes.forEach(anecdote => {
+            store.dispatch(createAnecdote(anecdote))
+        });
+    }
+)
+
 
 export default store
