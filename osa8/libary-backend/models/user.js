01232eabbc245f71d92b596030440e52ac58ec60
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-16 11:04:46 +0200

    started using a new mongodb table, fixing the duplicate username bug

--- osa8/libary-backend/models/user.js
+++ osa8/libary-backend/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -19,6 +19,6 @@ const userSchema = mongoose.Schema({
 
 
 
-module.exports = mongoose.model('libaryUser', userSchema)
+module.exports = mongoose.model('userLibary', userSchema)
 
 
