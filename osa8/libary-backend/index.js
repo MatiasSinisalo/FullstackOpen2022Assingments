e3cf5a6795f32f7aa84a96f1c6021b9d7b02b911
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-11-29 15:53:01 +0200

    started using express on backend and refactored some code to modules

--- osa8/libary-backend/index.js
+++ osa8/libary-backend/index.js
@@ -1,5 +1,9 @@
 require("dotenv").config()
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const { ApolloServer, UserInputError } = require('apollo-server-express')
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const http = require('http')
 const { UniqueDirectiveNamesRule } = require('graphql')
 const { mongoose } = require('mongoose')
 const { v4: uuid } = require('uuid')
@@ -9,8 +13,9 @@ const Author = require('./models/author')
 const Book = require('./models/book')
 const User = require('./models/user')
 
-let url = process.env.MONGODB_URI
-mongoose.connect(url)
+const typeDefs = require('./schema')
+const resolvers = require('./resorvers')
+mongoose.connect(process.env.MONGODB_URI)
 
 
 
@@ -111,251 +116,47 @@ let booksDepricated = [
 
 
 
+const start = async () => {
 
-const typeDefs = gql`
+  const app = express()
+  const httpServer = http.createServer(app)
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-  type Author {
-    name: String!
-    id: ID!
-    born: Int
-    bookCount: Int
- }
-
- type Book {
-  title: String!
-  published: Int!
-  author: Author!
-  genres: [String!]!
-  id: ID!
-}
-
-type User {
-  username: String!
-  favoriteGenre: String!
-  id: ID!
-}
-
-type Token {
-  value: String!
-}
-
-
-
-  type Mutation{
-    addBook(
-      title: String!
-      published: Int!
-      author: String!
-      genres: [String!]!
-    ):Book
-
-    editAuthor(
-      name: String!
-      setBornTo: Int!
-    ):Author
-
-
-    createUser(
-      username: String!
-      favoriteGenre: String!
-    ): User
-
-    login(
-      username: String!
-      password: String!
-    ): Token
-  }
-
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks (author: String, genre: String): [Book!]!
-    allAuthors: [Author!]!
-    me: User
-  }
-
-
-
-`
-
-
-const resolvers = {
- 
-  Query: {
-    authorCount: async() => {
-      const authors = await Author.find({})
-      return authors.length
-    },
-    bookCount: async () => {
-      const books = await Book.find({})
-      return books.length
-    },
-    allBooks: async (root, args) => {
-        const books = await Book.find({}).populate('author')
-        const booksByAuthor = args.author !== undefined ? books.filter((book) => book.author.name === args.author) : books
-        const booksByGenre = args.genre !== undefined ? booksByAuthor.filter((book) => book.genres.filter((genre) => genre === args.genre).length > 0) : booksByAuthor
-        return booksByGenre
-    },
-    allAuthors: async() => {
-      const authors = Author.find({})
-      return authors
-    },
-    me: async(root, args, context) => {
-      return context.user
-    }
-
-  },
-  Mutation: {
-    addBook: async (root, args, context) => {
-
-      if(context.user === undefined){
-        throw new UserInputError("Unauthorized", {
-          invalidArgs: args,
-        })
-      }
-
-      const book = {...args}
-      let author = await Author.findOne({name: book.author})//authors.filter((author) => author.name === book.author)
-    
-      if(author === null){
-        const newAuthor = {
-          name: book.author,
-          born: null,
-        
-        }
-       
-       
-        try{
-          const authorObj = Author(newAuthor)
-          const savedAuthor = await authorObj.save()
-          author = {
-            name: savedAuthor.name,
-            born: savedAuthor.born,
-            id: savedAuthor._id, //id is needed if the request wants the id back from create book query
-            _id: savedAuthor.id //_id is needed for the bookToSave.save() to have the correct ref to author
-          }
-        }
-        catch(error){
-          throw new UserInputError(error.message, {
-            invalidArgs: args,
-          })
-        }
-        
-      }
-      
-      const bookToSave = {...book, author: author}
-      console.log(bookToSave)
-     
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => {
+      //the try catch is there because i dont want to show an error in a situtation where the token is given but it is not needed
+      //for example in the case of login and createuser
       try{
-        const bookObj = Book(bookToSave)
-        const returnedBook = await bookObj.save()
-        console.log({...bookToSave, id: returnedBook._id})
-        return {...bookToSave, id: returnedBook._id}
-      }
-      catch(error){
-        throw new UserInputError(error.message, {
-          invalidArgs: args,
-        })
-      }
-    },
-    editAuthor: async (root, args, context) => {
-      const author =  await Author.findOne({name: args.name})
-      
-      if(author === null){
-        throw new UserInputError("Author not found", {
-          invalidArgs: args,
-        })
-      }
-      console.log(context.user)
-      if(context.user === undefined){
-        throw new UserInputError("Unauthorized", {
-          invalidArgs: args,
-        })
-      }
-
-
-      try{
-      const updatedAuthor = await Author.findByIdAndUpdate(author.id, {born: args.setBornTo}, {new: true})
-      return updatedAuthor
-      }
-      catch(error){
-        throw new UserInputError(error.message, {
-          invalidArgs: args,
-        })
-      }
-    },
-    createUser: async (root, args) => {
-      const hashedPassword = await bcrypt.hash('password12345', 10)
-      const newUser = {...args, passwordHash: hashedPassword} //this is intenttionally hardcoded
+      const authorizationHeader = req.headers.authorization
     
-     // console.log(newUser)
-     try{
-      const newUserObj = User(newUser)
-     // console.log(newUserObj)
-      const response = await newUserObj.save()
-      //console.log({username: response.username, favoriteGenre: response.favoriteGenre, id: response._id})
-      return {username: response.username, favoriteGenre: response.favoriteGenre, id: response._id}
-     }
-     catch(error){
-      throw new UserInputError(error.message, {
-        invalidArgs: "password or username are invalid",
-      })
-     }
-    },
-    login: async (root, args) => {
-      const proposedPassword = args.password
-      const userToCompareTo =  await User.findOne({username: args.username})
-      const correct = await bcrypt.compare(proposedPassword, userToCompareTo.passwordHash)
-      if(correct){
-        
-        const tokenInfo = {
-          username: userToCompareTo.username,
-          id: userToCompareTo.id
-        }
-        const token = await jwt.sign(tokenInfo, process.env.SECRET)
-        return {value: token}
+      if (authorizationHeader && authorizationHeader.toLowerCase().startsWith('bearer ')) {
+        const token = jwt.verify(authorizationHeader.substring(7), process.env.SECRET)
+        const user = await User.findById(token.id)
+      
+        return { user }
       }
-
-      throw new UserInputError("Incorrect credentials", {
-        invalidArgs: "password or username are invalid",
-      })
     }
-  },
-  Author: {
-    bookCount: async (root) => {
-        const books = await Book.find({author: root.id})
-        return books.length
-    } 
+    catch{
 
-  }
+    }
+    
+    },  
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
 
+  await server.start()
+
+  server.applyMiddleware({
+    app,
+    path: '/',
+  })
+
+  const PORT = process.env.PORT
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server ready at http://localhost:${PORT}`)
+  })
 }
 
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    //the try catch is there because i dont want to show an error in a situtation where the token is given but it is not needed
-    //for example in the case of login and createuser
-    try{
-    const authorizationHeader = req.headers.authorization
-   
-    if (authorizationHeader && authorizationHeader.toLowerCase().startsWith('bearer ')) {
-      const token = jwt.verify(authorizationHeader.substring(7), process.env.SECRET)
-      const user = await User.findById(token.id)
-     
-      return { user }
-    }
-  }
-  catch{
-
-  }
-  
-  
-  }
-})
-
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+start()
---
+++ osa8/libary-backend/resorvers.js
@@ -0,0 +1,158 @@
+const Author = require('./models/author')
+const Book = require('./models/book')
+const User = require('./models/user')
+
+
+const resolvers = {
+ 
+    Query: {
+      authorCount: async() => {
+        const authors = await Author.find({})
+        return authors.length
+      },
+      bookCount: async () => {
+        const books = await Book.find({})
+        return books.length
+      },
+      allBooks: async (root, args) => {
+          const books = await Book.find({}).populate('author')
+          const booksByAuthor = args.author !== undefined ? books.filter((book) => book.author.name === args.author) : books
+          const booksByGenre = args.genre !== undefined ? booksByAuthor.filter((book) => book.genres.filter((genre) => genre === args.genre).length > 0) : booksByAuthor
+          return booksByGenre
+      },
+      allAuthors: async() => {
+        const authors = Author.find({})
+        return authors
+      },
+      me: async(root, args, context) => {
+        return context.user
+      }
+  
+    },
+    Mutation: {
+      addBook: async (root, args, context) => {
+  
+        if(context.user === undefined){
+          throw new UserInputError("Unauthorized", {
+            invalidArgs: args,
+          })
+        }
+  
+        const book = {...args}
+        let author = await Author.findOne({name: book.author})//authors.filter((author) => author.name === book.author)
+      
+        if(author === null){
+          const newAuthor = {
+            name: book.author,
+            born: null,
+          
+          }
+         
+         
+          try{
+            const authorObj = Author(newAuthor)
+            const savedAuthor = await authorObj.save()
+            author = {
+              name: savedAuthor.name,
+              born: savedAuthor.born,
+              id: savedAuthor._id, //id is needed if the request wants the id back from create book query
+              _id: savedAuthor.id //_id is needed for the bookToSave.save() to have the correct ref to author
+            }
+          }
+          catch(error){
+            throw new UserInputError(error.message, {
+              invalidArgs: args,
+            })
+          }
+          
+        }
+        
+        const bookToSave = {...book, author: author}
+        console.log(bookToSave)
+       
+        try{
+          const bookObj = Book(bookToSave)
+          const returnedBook = await bookObj.save()
+          console.log({...bookToSave, id: returnedBook._id})
+          return {...bookToSave, id: returnedBook._id}
+        }
+        catch(error){
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        }
+      },
+      editAuthor: async (root, args, context) => {
+        const author =  await Author.findOne({name: args.name})
+        
+        if(author === null){
+          throw new UserInputError("Author not found", {
+            invalidArgs: args,
+          })
+        }
+        console.log(context.user)
+        if(context.user === undefined){
+          throw new UserInputError("Unauthorized", {
+            invalidArgs: args,
+          })
+        }
+  
+  
+        try{
+        const updatedAuthor = await Author.findByIdAndUpdate(author.id, {born: args.setBornTo}, {new: true})
+        return updatedAuthor
+        }
+        catch(error){
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        }
+      },
+      createUser: async (root, args) => {
+        const hashedPassword = await bcrypt.hash('password12345', 10)
+        const newUser = {...args, passwordHash: hashedPassword} //this is intenttionally hardcoded
+      
+       // console.log(newUser)
+       try{
+        const newUserObj = User(newUser)
+       // console.log(newUserObj)
+        const response = await newUserObj.save()
+        //console.log({username: response.username, favoriteGenre: response.favoriteGenre, id: response._id})
+        return {username: response.username, favoriteGenre: response.favoriteGenre, id: response._id}
+       }
+       catch(error){
+        throw new UserInputError(error.message, {
+          invalidArgs: "password or username are invalid",
+        })
+       }
+      },
+      login: async (root, args) => {
+        const proposedPassword = args.password
+        const userToCompareTo =  await User.findOne({username: args.username})
+        const correct = await bcrypt.compare(proposedPassword, userToCompareTo.passwordHash)
+        if(correct){
+          
+          const tokenInfo = {
+            username: userToCompareTo.username,
+            id: userToCompareTo.id
+          }
+          const token = await jwt.sign(tokenInfo, process.env.SECRET)
+          return {value: token}
+        }
+  
+        throw new UserInputError("Incorrect credentials", {
+          invalidArgs: "password or username are invalid",
+        })
+      }
+    },
+    Author: {
+      bookCount: async (root) => {
+          const books = await Book.find({author: root.id})
+          return books.length
+      } 
+  
+    }
+  
+  }
+
+module.exports = resolvers
