b660c5aea4e487497be9e74d90d5e1c53456f43a
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-12-02 09:30:05 +0200

    confiqured front end to be able to use subscriptions

--- osa8/library-frontend/src/index.js
+++ osa8/library-frontend/src/index.js
@@ -2,37 +2,66 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+
+
+
 import {
     ApolloClient,
     ApolloProvider,
     HttpLink,
     InMemoryCache,
+    split
   } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+
+
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 
 
 
 
 const authHeaderModification = setContext((_, { headers }) => {
-    const token = localStorage.getItem('libaryUserToken')
-    let authHeader = null
-    if(token){
-      authHeader = `bearer ${token}`
-    }
+  const token = localStorage.getItem('libaryUserToken')
+  let authHeader = null
+  if(token){
+    authHeader = `bearer ${token}`
+  }
+  return {
+      headers: {...headers, authorization: authHeader}
+  }
+})
 
-    return {
-        headers: {...headers, authorization: authHeader}
-    }
-  })
 
-  const httpLink = new HttpLink({
-    uri: 'http://localhost:4000',
-  })
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+})
 
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: authHeaderModification.concat(httpLink)
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:4000',
   })
+)
+
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  authHeaderModification.concat(httpLink)
+)
+
+ 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: splitLink
+})
 
 
 
